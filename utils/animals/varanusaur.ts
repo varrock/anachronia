5be7b6d015462576d86d8a9d83f24c5826e7a504
Dragonite <haolin97@gmail.com>
2020-06-07 22:44:30 +0800

    Add correct 2h cycle for Varanusaur

--- utils/animals/varanusaur.ts
+++ utils/animals/varanusaur.ts
@@ -2,7 +2,7 @@ import * as moment from 'moment'
 import Dinosaur from "../interfaces";
 
 // Initial Breeding Tick
-const initialTick = moment.utc(Date.UTC(2020, 5, 6, 10, 40))
+const initialTick = moment.utc(Date.UTC(2020, 5, 7, 0, 0))
 const iconURL = "https://runescape.wiki/images/1/10/Ripper_dinosaur_%28player-owned_farm%29.png"
 const imageURL = "https://runescape.wiki/images/thumb/5/51/Ripper_dinosaur_%28grown%29.png/412px-Ripper_dinosaur_%28grown%29.png"
 const wikiURL = "https://runescape.wiki/w/Dinosaur_(player-owned_farm)"
@@ -15,5 +15,5 @@ export const varanusaur: Dinosaur = {
     image: imageURL,
     wiki: wikiURL,
     breedingPenRate: 800,
-    regularPenRate: 2400
+    regularPenRate: 1600
 }
