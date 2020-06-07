e0cf043141d52d5019148c61bb3cdd19ee08be23
Dragonite <haolin97@gmail.com>
2020-06-07 22:47:08 +0800

    Update salamander to use correct 1.5 hour cycles

--- utils/animals/salamander.ts
+++ utils/animals/salamander.ts
@@ -2,7 +2,7 @@ import * as moment from 'moment'
 import Dinosaur from "../interfaces";
 
 // Initial Breeding Tick
-const initialTick = moment.utc(Date.UTC(2020, 5, 7, 2, 45))
+const initialTick = moment.utc(Date.UTC(2020, 5, 7, 0, 0))
 const iconURL = "https://runescape.wiki/images/0/04/Green_salamander_%28player-owned_farm%29.png"
 const imageURL = "https://runescape.wiki/images/thumb/e/e4/Green_salamander_%28NPC%29.png/301px-Green_salamander_%28NPC%29.png"
 const wikiURL = "https://runescape.wiki/w/Salamander_(player-owned_farm)"
@@ -14,6 +14,6 @@ export const salamander: Dinosaur = {
     icon: iconURL,
     image: imageURL,
     wiki: wikiURL,
-    breedingPenRate: 1005,
-    regularPenRate: 3015
+    breedingPenRate: 600,
+    regularPenRate: 900
 }
