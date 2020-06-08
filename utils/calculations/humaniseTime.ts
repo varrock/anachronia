9476a5779e6e6f3a40b3e8f07e39632fb3b1e015
Dragonite <haolin97@gmail.com>
2020-06-09 01:27:47 +0800

    Add conditional ETA to humaniseTime

--- utils/calculations/humaniseTime.ts
+++ utils/calculations/humaniseTime.ts
@@ -1,11 +1,12 @@
 import * as moment from 'moment';
 import {Moment} from 'moment';
 
-export default function humaniseTime(nextTick: Moment) {
+export default function humaniseTime(nextTick: Moment, eta: boolean = true) {
     const currentMoment = moment.utc();
     const partialHours = Math.floor(nextTick.diff(currentMoment, 'minutes')/60);
     const partialMinutes = nextTick.diff(currentMoment, 'minutes') % 60;
     let hoursString = partialHours ? `${partialHours} hours` : '';
+    const etaString = eta ? "**ETA**: " : "";
     if (partialHours === 1) {
         hoursString = hoursString.substring(0, hoursString.length - 1);
     }
@@ -14,12 +15,12 @@ export default function humaniseTime(nextTick: Moment) {
         minutesString = minutesString.substring(0, minutesString.length - 1);
     }
     if (hoursString && minutesString) {
-        return `**ETA**: *${hoursString}, ${minutesString}*`;
+        return etaString + `*${hoursString}, ${minutesString}*`;
     } else if (hoursString && !minutesString) {
-        return `**ETA**: *${hoursString}*`;
+        return etaString + `*${hoursString}*`;
     } else if (!hoursString && minutesString) {
-        return `**ETA**: *${minutesString}*`;
+        return etaString + `*${minutesString}*`;
     } else {
-        return "**ETA**: *The breed tick is now*";
+        return etaString + "*The breed tick is now*";
     }
 }
