b612193c1700df1267eaf15efe7c578ae9a6798b
Haolin Wu <haolin.wu@uwa.edu.au>
2020-06-08 23:05:08 +0800

    Plural on minutes and hours fix

--- utils/calculations/humaniseTime.ts
+++ utils/calculations/humaniseTime.ts
@@ -5,8 +5,14 @@ export default function humaniseTime(nextTick: Moment) {
     const currentMoment = moment.utc();
     const partialHours = Math.floor(nextTick.diff(currentMoment, 'minutes')/60);
     const partialMinutes = nextTick.diff(currentMoment, 'minutes') % 60;
-    const hoursString = partialHours ? `${partialHours} hours` : '';
-    const minutesString = partialMinutes ? `${partialMinutes} minutes` : '';
+    let hoursString = partialHours ? `${partialHours} hours` : '';
+    if (partialHours === 1) {
+        hoursString = hoursString.substring(0, hoursString.length - 1);
+    }
+    let minutesString = partialMinutes ? `${partialMinutes} minutes` : '';
+    if (partialMinutes === 1) {
+        minutesString = minutesString.substring(0, minutesString.length - 1);
+    }
     if (hoursString && minutesString) {
         return `**ETA**: *${hoursString}, ${minutesString}*`;
     } else if (hoursString && !minutesString) {
