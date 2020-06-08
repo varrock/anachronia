743d8c9d9cfc125d2273f018cdda1a7d1697856d
Dragonite <haolin97@gmail.com>
2020-06-09 01:28:06 +0800

    Add sorted Regular pen logic

--- utils/calculations/sortedDinosaurTime.ts
+++ utils/calculations/sortedDinosaurTime.ts
@@ -1,60 +1,17 @@
 import nextBreedingTick from "./breeding";
 import nextRegularTick from "./regular";
+import humaniseTime from "./humaniseTime";
 import {
     pavosaurus,
-    brutish,
     arcane,
-    scimitops,
-    jadinko,
     varanusaur,
     salamander,
     frog,
     malletops,
-    oculi,
     spicati,
-    asciatops,
-    bagrada,
-    corbicula
-} from "../animals"
+} from "../animals";
 
 // We only need to sort 7 'sets' of dinosaurs instead of the full 14 as some share ticks.
-
-// let unsortedArray = []
-// unsortedArray.push({
-//     name: frog.name,
-//     nextBreeding: nextBreedingTick(frog),
-//     nextLarge: nextRegularTick(frog)
-// })
-// unsortedArray.push({
-//     name: salamander.name,
-//     nextBreeding: nextBreedingTick(salamander),
-//     nextLarge: nextRegularTick(salamander)
-// })
-// unsortedArray.push({
-//     name: pavosaurus.name,
-//     nextBreeding: nextBreedingTick(pavosaurus),
-//     nextLarge: nextRegularTick(pavosaurus)
-// })
-// unsortedArray.push({
-//     name: "Asciatops/Bagrada/Corbicula/Spicati",
-//     nextBreeding: nextBreedingTick(spicati),
-//     nextLarge: nextRegularTick(malletops)
-// })
-// unsortedArray.push({
-//     name: "Malletops/Oculi",
-//     nextBreeding: nextBreedingTick(malletops),
-//     nextLarge: nextRegularTick(malletops)
-// })
-// unsortedArray.push({
-//     name: "Arcane/Brutish/Scimitops",
-//     nextBreeding: nextBreedingTick(arcane),
-//     nextLarge: nextRegularTick(arcane)
-// })
-// unsortedArray.push({
-//     name: varanusaur.name,
-//     nextBreeding: nextBreedingTick(varanusaur),
-//     nextLarge: nextRegularTick(varanusaur)
-// })
 
 function sortedBreedingTicks() {
     let unsortedArray = []
@@ -87,21 +44,50 @@ function sortedBreedingTicks() {
         nextBreeding: nextBreedingTick(varanusaur)
     })
     let sortedArray = unsortedArray.sort((a,b) => a.nextBreeding.diff(b.nextBreeding))
+    sortedArray.forEach(element => {
+        element.nextBreeding = humaniseTime(element.nextBreeding, false)
+    })
+    return sortedArray
+}
+
+function sortedRegularTicks() {
+    let unsortedArray = []
+    unsortedArray.push({
+        name: frog.name,
+        nextRegular: nextRegularTick(frog)
+    })
+    unsortedArray.push({
+        name: salamander.name,
+        nextRegular: nextRegularTick(salamander)
+    })
+    unsortedArray.push({
+        name: pavosaurus.name,
+        nextRegular: nextRegularTick(pavosaurus)
+    })
+    unsortedArray.push({
+        name: "Asciatops/Bagrada/Corbicula/Spicati",
+        nextRegular: nextRegularTick(malletops)
+    })
+    unsortedArray.push({
+        name: "Malletops/Oculi",
+        nextRegular: nextRegularTick(malletops)
+    })
+    unsortedArray.push({
+        name: "Arcane/Brutish/Scimitops",
+        nextRegular: nextRegularTick(arcane)
+    })
+    unsortedArray.push({
+        name: varanusaur.name,
+        nextRegular: nextRegularTick(varanusaur)
+    })
+    let sortedArray = unsortedArray.sort((a,b) => a.nextRegular.diff(b.nextRegular))
+    sortedArray.forEach(element => {
+        element.nextRegular = humaniseTime(element.nextRegular, false)
+    })
     return sortedArray
 }
 
 export {
-    sortedBreedingTicks
+    sortedBreedingTicks,
+    sortedRegularTicks
 }
-
-
-// export default interface Dinosaur {
-//     readonly name: string
-//     readonly penType: string
-//     readonly initial: Moment
-//     readonly icon: string
-//     readonly image: string
-//     readonly wiki: string
-//     readonly breedingPenRate: number
-//     readonly regularPenRate: number
-// }
