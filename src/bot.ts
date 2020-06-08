03055d5e9f62c3c084300b5bbeac336f6253e3b6
Dragonite <haolin97@gmail.com>
2020-06-09 00:31:31 +0800

    Move messageResponder import into index

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
 import {Client, Message} from "discord.js";
 import {inject, injectable} from "inversify";
 import {TYPES} from "./types";
-import {MessageResponder} from "./services/messageResponder";
+import {MessageResponder} from "./services";
 import {prefix} from '../utils';
 
 @injectable()
---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import {MessageResponder} from "./messageResponder";
+
+export {
+    MessageResponder
+}
