cf357426a31f4ecce84a163107ce2fd18214e0a8
Dragonite <haolin97@gmail.com>
2020-06-09 00:25:51 +0800

    Refractor dinosaur finders

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,17 +1,17 @@
-import {PavosaurusFinder} from './pavosaurusFinder'
-import {BrutishFinder} from './brutishFinder'
-import {ArcaneFinder} from "./arcaneFinder";
-import {ScimitopFinder} from "./scimitopFinder";
-import {JadinkoFinder} from "./jadinkoFinder";
-import {VaranusaurFinder} from "./varanusaurFinder";
-import {SalamanderFinder} from "./salamanderFinder";
-import {FrogFinder} from "./frogFinder";
-import {MalletopsFinder} from "./malletopsFinder";
-import {OculiFinder} from "./oculiFinder";
-import {SpicatiFinder} from "./spicatiFinder";
-import {AsciatopsFinder} from "./asciatopsFinder";
-import {BagradaFinder} from "./bagradaFinder";
-import {CorbiculaFinder} from "./corbiculaFinder";
+import {PavosaurusFinder} from './dinoFinders/pavosaurusFinder'
+import {BrutishFinder} from './dinoFinders/brutishFinder'
+import {ArcaneFinder} from "./dinoFinders/arcaneFinder";
+import {ScimitopFinder} from "./dinoFinders/scimitopFinder";
+import {JadinkoFinder} from "./dinoFinders/jadinkoFinder";
+import {VaranusaurFinder} from "./dinoFinders/varanusaurFinder";
+import {SalamanderFinder} from "./dinoFinders/salamanderFinder";
+import {FrogFinder} from "./dinoFinders/frogFinder";
+import {MalletopsFinder} from "./dinoFinders/malletopsFinder";
+import {OculiFinder} from "./dinoFinders/oculiFinder";
+import {SpicatiFinder} from "./dinoFinders/spicatiFinder";
+import {AsciatopsFinder} from "./dinoFinders/asciatopsFinder";
+import {BagradaFinder} from "./dinoFinders/bagradaFinder";
+import {CorbiculaFinder} from "./dinoFinders/corbiculaFinder";
 
 export {
     PavosaurusFinder,
