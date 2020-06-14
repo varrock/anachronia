4209537d0050942ad7339d6cb96a685866d552f6
Haolin Wu <haolin.wu@uwa.edu.au>
2020-06-14 21:52:23 +0800

    Use new dinosaur Interface constant and optional arguments

--- utils/calculations/sortedDinosaurTime.ts
+++ utils/calculations/sortedDinosaurTime.ts
@@ -1,54 +1,13 @@
-import nextBreedingTick from "./breeding";
-import nextRegularTick from "./regular";
 import humaniseTime from "./humaniseTime";
-import {
-    pavosaurus,
-    arcane,
-    jadinko,
-    varanusaur,
-    salamander,
-    frog,
-    malletops,
-    spicati,
-} from "../animals";
-import {DinosaurElement, DinosaurMoment, TickElement} from "../interfaces";
+import {Message} from "discord.js";
+import {totalInterfaceList} from "../constants";
+import {Dinosaur, DinosaurElement, DinosaurMoment, TickElement} from "../interfaces";
+import {getInterfacesFromRoles, buildTickString, generateUnsortedArray} from "./index";
 
 // We only need to sort 7 'sets' of dinosaurs instead of the full 14 as some share ticks.
 
-function sortedBreedingTicks(): DinosaurElement[] {
-    let unsortedArray: DinosaurMoment[] = []
-    unsortedArray.push({
-        name: frog.name,
-        value: nextBreedingTick(frog)
-    })
-    unsortedArray.push({
-        name: salamander.name,
-        value: nextBreedingTick(salamander)
-    })
-    unsortedArray.push({
-        name: jadinko.name,
-        value: nextBreedingTick(jadinko)
-    })
-    unsortedArray.push({
-        name: pavosaurus.name,
-        value: nextBreedingTick(pavosaurus)
-    })
-    unsortedArray.push({
-        name: "Asciatops/Bagrada/Corbicula/Spicati",
-        value: nextBreedingTick(spicati)
-    })
-    unsortedArray.push({
-        name: "Malletops/Oculi",
-        value: nextBreedingTick(malletops)
-    })
-    unsortedArray.push({
-        name: "Arcane/Brutish/Scimitops",
-        value: nextBreedingTick(arcane)
-    })
-    unsortedArray.push({
-        name: varanusaur.name,
-        value: nextBreedingTick(varanusaur)
-    })
+function sortedBreedingTicks(interfaceList: Dinosaur[] = totalInterfaceList): DinosaurElement[] {
+    let unsortedArray: DinosaurMoment[] = generateUnsortedArray(interfaceList, true)
     let sortedMomentArray = unsortedArray.sort((a: DinosaurMoment, b: DinosaurMoment) => a.value.diff(b.value))
     let sortedArray: DinosaurElement[] = sortedMomentArray.map(item => ({
         name: item.name,
@@ -57,54 +16,14 @@ function sortedBreedingTicks(): DinosaurElement[] {
     return sortedArray
 }
 
-function sortedRegularTicks(): DinosaurElement[] {
-    let unsortedArray: DinosaurMoment[] = []
-    unsortedArray.push({
-        name: frog.name,
-        value: nextRegularTick(frog)
-    })
-    unsortedArray.push({
-        name: salamander.name,
-        value: nextRegularTick(salamander)
-    })
-    unsortedArray.push({
-        name: jadinko.name,
-        value: nextRegularTick(jadinko)
-    })
-    unsortedArray.push({
-        name: pavosaurus.name,
-        value: nextRegularTick(pavosaurus)
-    })
-    unsortedArray.push({
-        name: "Asciatops/Bagrada/Corbicula/Spicati",
-        value: nextRegularTick(malletops)
-    })
-    unsortedArray.push({
-        name: "Malletops/Oculi",
-        value: nextRegularTick(malletops)
-    })
-    unsortedArray.push({
-        name: "Arcane/Brutish/Scimitops",
-        value: nextRegularTick(arcane)
-    })
-    unsortedArray.push({
-        name: varanusaur.name,
-        value: nextRegularTick(varanusaur)
-    })
+function sortedRegularTicks(interfaceList: Dinosaur[] = totalInterfaceList): DinosaurElement[] {
+    let unsortedArray: DinosaurMoment[] = generateUnsortedArray(interfaceList, false)
     let sortedMomentArray = unsortedArray.sort((a: DinosaurMoment, b: DinosaurMoment) => a.value.diff(b.value))
     let sortedArray: DinosaurElement[] = sortedMomentArray.map(item => ({
         name: item.name,
         value: humaniseTime(item.value, false)
     }))
     return sortedArray
-}
-
-function buildTickString(sortedTicks: DinosaurElement[]): string {
-    let tickString = "";
-    sortedTicks.forEach(element => {
-        tickString += `**${element.name}** - ${element.value}\n`
-    })
-    return tickString
 }
 
 function sortedTicks(): TickElement[] {
@@ -119,6 +38,12 @@ function sortedTicks(): TickElement[] {
             inline: false
         }
     ]
+
+}
+
+function sortedPersonalTicks(message: Message) {
+    const interfaceList = getInterfacesFromRoles(message);
+
 
 }
 
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,10 @@
-export const prefix = "!"
+import {arcane, frog, jadinko, malletops, pavosaurus, salamander, spicati, varanusaur} from "./animals"
+
+const totalInterfaceList = [frog, salamander, jadinko, varanusaur, arcane, spicati, malletops, pavosaurus]
+
+const prefix = "!";
+
+export {
+    prefix,
+    totalInterfaceList
+}
